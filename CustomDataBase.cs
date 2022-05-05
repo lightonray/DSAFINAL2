3b80816f6aa7294b0ee81cf8c2b72a2a17bdd16f
rei kasemi <reikasemi2001@gmail.com>
2022-05-05 22:17:24 +0300

    Add project files.

---
+++ CustomDataBase.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lightDSAFINAL
+{
+    class CustomDataBase
+    {
+        private LinkedList<Student> dataBase;
+
+
+        public CustomDataBase()
+        {
+            this.dataBase = new LinkedList<Student>();
+        }
+
+        public LinkedList<Student> GetDataBase
+        {
+            get { return dataBase; }
+        }
+
+        public void PopulateWithSampleStudent()
+        {
+            float[] score1 = { 40, 60, 70, 80 };
+            Student student1 = new Student("Rei", "Kasemi", "2117024", score1);
+            float[] score2 = { 10, 20, 30, 40 };
+            Student student2 = new Student("Dario", "Lulashi", "2117025", score2);
+            float[] score3 = { 70, 80, 90, 100 };
+            Student student3 = new Student("Murat", "Dedolli", "2117026", score3);
+            float[] score4 = { 24, 67, 32, 44 };
+            Student student4 = new Student("Erni", "Cullhaj", "2117027", score4);
+            dataBase.AddFirst(student1);
+            dataBase.AddFirst(student2);
+            dataBase.AddFirst(student3);
+            dataBase.AddFirst(student4);
+        }
+
+        public void Add(Student student)
+        {
+            dataBase.AddLast(student);
+        }
+
+        public void GetElementByIndex(int index)
+        {
+            Console.WriteLine(dataBase.ElementAt(index).ToString());
+        }
+
+        public void RemoveByIndex(int index)
+        {
+            dataBase.Remove(dataBase.ElementAt(index));
+        }
+
+        public void RemoveFirst()
+        {
+            dataBase.RemoveFirst();
+        }
+        public void RemoveLast()
+        {
+            dataBase.RemoveLast();
+        }
+
+        public void DisplayList()
+        {
+            if (dataBase.Count != 0)
+            {
+                Console.WriteLine("These are the differents informations about your student :");
+                Console.WriteLine();
+                foreach (var student in dataBase)
+                {
+                    Console.WriteLine(student.ToString());
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("There is no student in the list ");
+            }
+        }
+
+        public void Sort(string sortDirection, int sortField)
+        {
+            if (sortField == 1) //i used this sort because we don't have many students in the data structure, but if we had more students we should use a more optimized one
+            {
+                if (sortDirection == "asc") // by first name ascendant 
+                {
+                    int inerator = 0;
+                    int n = dataBase.Count - 1;
+                    int letter = 0;
+                    while (inerator < dataBase.Count)
+                    {
+                        for (int i = n; i >= 1; i--)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                while (dataBase.ElementAt(j - 1).GetFirstName[letter] == dataBase.ElementAt(j).GetFirstName[letter])
+                                {
+                                    letter++;
+                                }
+                                if (dataBase.ElementAt(j - 1).GetFirstName[letter] > dataBase.ElementAt(j).GetFirstName[letter])
+                                {
+                                    Student temp = dataBase.ElementAt(j - 1);
+                                    dataBase.Remove(dataBase.ElementAt(j - 1));
+                                    dataBase.AddLast(temp);
+                                    inerator = 0;                                    
+                                }
+                                inerator++;
+                                letter = 0;
+                            }
+                        }
+                    }
+                }
+
+
+                if (sortDirection == "desc") // by first name descendant 
+                {
+                    int inerator = 0;
+                    int n = dataBase.Count - 1;
+                    int letter = 0;
+                    while (inerator < dataBase.Count)
+                    {
+                        for (int i = n; i >= 0; i--)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                while (dataBase.ElementAt(j - 1).GetFirstName[letter] == dataBase.ElementAt(j).GetFirstName[letter])
+                                {
+                                    letter++;
+                                }
+                                if (dataBase.ElementAt(j - 1).GetFirstName[letter] < dataBase.ElementAt(j).GetFirstName[letter])
+                                {
+                                    Student temp = dataBase.ElementAt(j - 1);
+                                    dataBase.Remove(dataBase.ElementAt(j - 1));
+                                    dataBase.AddLast(temp);
+                                    inerator = 0;
+                                }
+                                inerator++;
+                                letter = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            if (sortField == 2) // LAST NAME
+            {
+                if (sortDirection == "asc") // by last name ascendant 
+                {
+                    int inerator = 0;
+                    int n = dataBase.Count - 1;
+                    int letter = 0;
+                    while (inerator < dataBase.Count)
+                    {
+                        for (int i = n; i >= 1; i--)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                while (dataBase.ElementAt(j - 1).GetLastName[letter] == dataBase.ElementAt(j).GetLastName[letter])
+                                {
+                                    letter++;
+                                }
+                                if (dataBase.ElementAt(j - 1).GetLastName[letter] > dataBase.ElementAt(j).GetLastName[letter])
+                                {
+                                    Student temp = dataBase.ElementAt(j - 1);
+                                    dataBase.Remove(dataBase.ElementAt(j - 1));
+                                    dataBase.AddLast(temp);
+                                    inerator = 0;
+                                }
+                                inerator++;
+                                letter = 0;
+                            }
+                        }
+                    }
+                }
+
+
+                if (sortDirection == "desc") // by last name descendant 
+                {
+                    int inerator = 0;
+                    int n = dataBase.Count - 1;
+                    int letter = 0;
+                    while (inerator < dataBase.Count)
+                    {
+                        for (int i = n; i >= 0; i--)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                while (dataBase.ElementAt(j - 1).GetLastName[letter] == dataBase.ElementAt(j).GetLastName[letter])
+                                {
+                                    letter++;
+                                }
+                                if (dataBase.ElementAt(j - 1).GetLastName[letter] < dataBase.ElementAt(j).GetLastName[letter])
+                                {
+                                    Student temp = dataBase.ElementAt(j - 1);
+                                    dataBase.Remove(dataBase.ElementAt(j - 1));
+                                    dataBase.AddLast(temp);
+                                    inerator = 0;
+                                }
+                                inerator++;
+                                letter = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            if (sortField == 3) // AVERAGE SCORE
+            {
+                if (sortDirection == "asc") // by AVERAGE ascendant 
+                {
+                    int inerator = 0;
+                    int n = dataBase.Count - 1;
+                    while (inerator < dataBase.Count)
+                    {
+                        for (int i = n; i >= 1; i--)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                if (dataBase.ElementAt(j - 1).GetAverageScore > dataBase.ElementAt(j).GetAverageScore)
+                                {
+                                    Student temp = dataBase.ElementAt(j - 1);
+                                    dataBase.Remove(dataBase.ElementAt(j - 1));
+                                    dataBase.AddLast(temp);
+                                    inerator = 0;
+                                }
+                                inerator++;
+                            }
+                        }
+                    }
+                }
+
+
+                if (sortDirection == "desc") // by last name descendant 
+                {
+                    int inerator = 0;
+                    int n = dataBase.Count - 1;
+                    while (inerator < dataBase.Count)
+                    {
+                        for (int i = n; i >= 0; i--)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                if (dataBase.ElementAt(j - 1).GetAverageScore < dataBase.ElementAt(j).GetAverageScore)
+                                {
+                                    Student temp = dataBase.ElementAt(j - 1);
+                                    dataBase.Remove(dataBase.ElementAt(j - 1));
+                                    dataBase.AddLast(temp);
+                                    inerator = 0;
+                                }
+                                inerator++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void GetMaxElement()
+        {
+            if (dataBase.Count != 0)
+            {
+                Student studentMax = dataBase.ElementAt(0);
+                foreach (var student in dataBase)
+                {
+                    if (student.GetAverageScore > studentMax.GetAverageScore)
+                    {
+                        studentMax = student;
+                    }
+                }
+                Console.WriteLine(studentMax.GetFullName + " has the highest average score, with an average of " + studentMax.GetAverageScore + ".");
+            }
+            else
+            {
+                Console.WriteLine("There is no student in the list ");
+            }
+        }
+
+        public void GetMinElement()
+        {
+            if (dataBase.Count != 0)
+            {
+                Student studentMin = dataBase.ElementAt(0);
+                foreach (var student in dataBase)
+                {
+                    if (student.GetAverageScore < studentMin.GetAverageScore)
+                    {
+                        studentMin = student;
+                    }
+                }
+                Console.WriteLine(studentMin.GetFullName + " has the lowest average score, with an average of " + studentMin.GetAverageScore + ".");
+            }
+            else
+            {
+                Console.WriteLine("There is no student in the list ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+
+namespace lightDSAFINAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomDataBase dataBase = new CustomDataBase();
+            int answer;
+            bool escape = false;
+            while (escape == false)
+            {
+                if (dataBase.GetDataBase.Count() != 0)
+                {
+                    Console.WriteLine("Student list :");
+                    Console.WriteLine();
+                    int count = 0;
+                    foreach (var student in dataBase.GetDataBase)
+                    {
+                        Console.WriteLine(count + ": " + student.GetFullName);
+                        count++;
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Empty list");
+                }
+                Console.WriteLine();
+                Console.WriteLine("------------------------------------------------------------------------");
+                Console.WriteLine("What do you want to do ?");
+                Console.WriteLine();
+                Console.WriteLine("Tape 1 to POPULATE with sample students");
+                Console.WriteLine("Tape 2 to ADD a student");
+                Console.WriteLine("Tape 3 to SEARCH and DISPLAY some informations of a student");
+                Console.WriteLine("Tape 4 to REMOVE a student by index");
+                Console.WriteLine("Tape 5 to REMOVE the first student in the list");
+                Console.WriteLine("Tape 6 to REMOVE the last student in the list");
+                Console.WriteLine("Tape 7 to DISPLAY all the informations of all your students");
+                Console.WriteLine("Tape 8 to SORT all your students");
+                Console.WriteLine("Tape 9 to GET STUDENT with BEST SCORE");
+                Console.WriteLine("Tape 10 to GET STUDENT with WORSE SCORE");
+                Console.WriteLine("Tape 0 to STOP the program");
+                Console.WriteLine("------------------------------------------------------------------------");
+                Console.WriteLine();
+                answer = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (answer)
+                {
+                    case 1: dataBase.PopulateWithSampleStudent(); break;
+                    case 2:
+                        {
+                            Console.WriteLine("First name :");
+                            string firstName = Convert.ToString(Console.ReadLine());
+                            Console.WriteLine("Last name :");
+                            string lastName = Convert.ToString(Console.ReadLine());
+                            Console.WriteLine("Student number :");
+                            string studentNumber = Convert.ToString(Console.ReadLine());
+                            Console.WriteLine("How many scores will you enter ?");
+                            int numberOfScores = Convert.ToInt32(Console.ReadLine());
+                            float[] studentScores = new float[numberOfScores];
+                            Console.WriteLine("Enter all the scores pushing enter between all of them ");
+                            for (int i = 0; i < numberOfScores; i++)
+                            {
+                                int score = Convert.ToInt32(Console.ReadLine());
+                                studentScores[i] = score;
+                            }
+                            Student student = new Student(firstName, lastName, studentNumber, studentScores);
+                            dataBase.Add(student);
+                        }
+                        break;
+
+                    case 3:
+                        {
+                            Console.WriteLine("Give me the index to display some informations about a specific student ");
+                            int index = Convert.ToInt32(Console.ReadLine());
+                            dataBase.GetElementByIndex(index);
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine("Give me the index to remove a specific student ");
+                            int index = Convert.ToInt32(Console.ReadLine());
+                            dataBase.RemoveByIndex(index);
+                        }
+                        ; break;
+                    case 5:
+                        {
+                            dataBase.RemoveFirst();
+                        }
+                        ; break;
+                    case 6:
+                        {
+                            dataBase.RemoveLast();
+                        }
+                        ; break;
+                    case 7:
+                        {
+                            dataBase.DisplayList();
+                        }
+                        ; break;
+                    case 8:
+                        {
+                            Console.WriteLine("Enter 1 to sort by first name ");
+                            Console.WriteLine("Enter 2 to sort by Last name ");
+                            Console.WriteLine("Enter 3 to sort by scores ");
+                            Console.WriteLine();
+                            int sortField = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter 'asc' for ascending sorting ");
+                            Console.WriteLine("Enter 'desc' descending sorting ");
+                            string sortDirection = Convert.ToString(Console.ReadLine());
+                            dataBase.Sort(sortDirection, sortField);
+                        }
+                        ; break;
+                    case 9:
+                        {
+                            dataBase.GetMaxElement();
+                        }
+                        ; break;
+                    case 10:
+                        {
+                            dataBase.GetMinElement();
+                        }
+                        ; break;
+                    case 0: escape = true; break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
